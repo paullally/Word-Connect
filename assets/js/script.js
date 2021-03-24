55e118c0c69a7d43981223daba4586229faa12d0
paullally <paullally98@gmail.com>
2021-03-24 15:33:19 +0000

    Add function that highlights cards clicked and checks if they are equal to answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,21 +9,17 @@ function shuffle(sourceArray) {
     }
     return sourceArray;
 }
-
-
-
-
-  
 function Start() {
     var random = Math.floor(Math.random() * 10);
 var random1 = Math.floor(Math.random() * 10);
 var random2 = Math.floor(Math.random() * 10);
 var random3 = Math.floor(Math.random() * 10);
 $.getJSON("game.json", function(json) {
-    var answer1 = [json[random].first,json[random].second,json[random].third,json[random].Link]
+    var answer1 = [json[random].first,json[random].second,json[random].third,json[random].fourth]
     var answer2 = [json[random1].first,json[random1].second,json[random1].third,json[random1].Link]
     var answer3 = [json[random2].first,json[random2].second,json[random2].third,json[random2].Link]
     var answer4 =  [json[random3].first,json[random3].second,json[random3].third,json[random3].Link]
+    console.log(answer1)
     var mix = [json[random].first,json[random].second,json[random].third,json[random].Link,json[random1].first,json[random1].second,json[random1].third,json[random1].Link,json[random2].first,json[random2].second,json[random2].third,json[random2].Link,json[random3].first,json[random3].second,json[random3].third,json[random3].Link]
     var mixed = shuffle(mix);
     let skill_list = "";
@@ -35,10 +31,39 @@ $.getJSON("game.json", function(json) {
         i += 1;
 
     });
-    console.log(skill_list)
      $("#start").addClass("hidden");
     $(".test").append(skill_list);
-  
+    let check =[]
+    $(".card").click(function() {
+        if( $(this).css("background-color")=="rgb(128, 128, 128)")
+        {
+            check.push(this.innerHTML)
+             $(this).css("background-color", "yellow");
+        }
+        else if ( $(this).css("background-color")=="rgb(255, 255, 0)")
+        {
+            $(this).css("background-color", "grey");
+             check.pop(this.innerHTML)
+        }   
+        console.log(check.length)
+        console.log(check)
+        if(check.length==4)
+        {
+            $(".card").css("background-color", "grey");
+            let count =0;
+            for(var i =0; i<check.length; i++)
+            {
+                    if(answer1.includes(check[i]))
+                    {
+                        count=count+1
+                    }
+            }
+            if(count==4)
+            {
+                console.log("correct")
+            }
+        }
+        
 });
-
+});
 }
