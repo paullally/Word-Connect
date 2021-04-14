4963b9862e6c7d8eadae5463e0a11a6df173fd77
paullally <paullally98@gmail.com>
2021-04-14 16:23:47 +0000

    add colour change to correct answers which pushes answer to top of game cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,9 +16,10 @@ var random2 = Math.floor(Math.random() * 10);
 var random3 = Math.floor(Math.random() * 10);
 $.getJSON("game.json", function(json) {
     var answer1 = [json[random].first,json[random].second,json[random].third,json[random].fourth]
-    var answer2 = [json[random1].first,json[random1].second,json[random1].third,json[random1].Link]
-    var answer3 = [json[random2].first,json[random2].second,json[random2].third,json[random2].Link]
-    var answer4 =  [json[random3].first,json[random3].second,json[random3].third,json[random3].Link]
+    var answer2 = [json[random1].first,json[random1].second,json[random1].third,json[random1].fourth]
+    var answer3 = [json[random2].first,json[random2].second,json[random2].third,json[random2].fourth]
+    var answer4 =  [json[random3].first,json[random3].second,json[random3].third,json[random3].fourth]
+    console.log(answer2)
     console.log(answer1)
     var mix = [json[random].first,json[random].second,json[random].third,json[random].Link,json[random1].first,json[random1].second,json[random1].third,json[random1].Link,json[random2].first,json[random2].second,json[random2].third,json[random2].Link,json[random3].first,json[random3].second,json[random3].third,json[random3].Link]
     var mixed = shuffle(mix);
@@ -47,17 +48,33 @@ $.getJSON("game.json", function(json) {
         }   
         console.log(check.length)
         console.log(check)
+
         if(check.length==4)
         {
             let count =0;
+            let count2 =0;
+            let count3 =0;
+            let count4 =0;
             for(var i =0; i<check.length; i++)
             {
                     if(answer1.includes(check[i]))
                     {
                         count=count+1
                     }
+                     if(answer2.includes(check[i]))
+                    {
+                        count2=count2+1
+                    }
+                    if(answer3.includes(check[i]))
+                    {
+                        count3=count3+1
+                    }
+                    if(answer4.includes(check[i]))
+                    {
+                        count4=count4+1
+                    }
             }
-            if(count==4)
+            if(count==4 || count2==4 || count3==4 || count4==4) 
             {
                 console.log("correct")
                 check=[];
@@ -67,6 +84,7 @@ $.getJSON("game.json", function(json) {
                     if($(".card").eq(i).css("background-color")=="rgb(255, 255, 0)")
                     {
                          $(".card").eq(i).css("background-color","green")
+                          $(".test").prepend( $(".card").eq(i));
                     }
                 }
             }
