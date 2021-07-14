66fcc477cd942bec055d6d111804fc6146476f05
paullally <paullally98@gmail.com>
2021-07-14 10:49:09 +0000

    Fix console error because of js

--- assets/js/script.js
+++ assets/js/script.js
@@ -419,15 +419,4 @@ $(window).keydown(function(event){
 });
 
 
-// this is the form submission it will connect to the google sheet and post the form data to it through the google sheet api
-const scriptURL = 'https://script.google.com/macros/s/AKfycbxKsfIj1rg8sy73Sp08BkOJJRMa0RRqt_lMPlc5aHx_3PoDJvCzG1My7600GWjna6gUpA/exec';
-            const form = document.forms['google-sheet'];
-            form.addEventListener('submit', e => {
-              e.preventDefault();
-              fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-                .then(response => setTimeout(function(){ document.getElementById('reply').classList.remove("hidden"); document.getElementById('myForm').classList.add("hidden");}, 1))
-                .catch(error => console.error('Error!', error.message));
-            });
-
-
         
